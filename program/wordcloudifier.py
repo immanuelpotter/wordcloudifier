56840bd49d6f427ddc38969d7d629134fe6aa9fb
immanuelpotter <mannypotter@hotmail.com>
2018-08-20 12:55:05 +0100

    Now in OO form

--- program/wordcloudifier.py
+++ program/wordcloudifier.py
@@ -1,49 +1,56 @@
 #!/usr/bin/env python
 #Take in user's long string of input, run the text against nltk, spit out a wordcloud
 import re
-import wordcloud as wc
+import wordcloud as wcld
 import matplotlib.pyplot as plt
 
-def read_stopwords(stopwords_file):
-    try:
-        stopwords = open(stopwords_file).read()
-        stopwords_list = stopwords.split()
-        return stopwords_list
-    except IOError:
-        return "That's not a text file!"
+class Wordcloudifier:
+    def __init__(self,stopwords,user_words):
+        try:
+            stopwords_list = open(stopwords).read().split()
+            self.stopwords_list = stopwords_list
+            self.user_words = None
+        except IOError:
+            return "That's not a file!"
 
-def user_text_and_clean():
-    text = str(raw_input("Enter your text here.\n"))
-    lowercase = text.lower()
-    noPunc = re.sub(r'[-./?!,":;()\']', ' ', lowercase)
-    noPuncSplit = noPunc.split()
-    return noPuncSplit
+    def user_text_and_clean(self):
+        text = str(raw_input("Enter your text here.\n"))
+        lowercase = text.lower()
+        noPunc = re.sub(r'[-./?!,":;()\']', ' ', lowercase)
+        noPuncSplit = noPunc.split()
+        self.user_words = noPuncSplit
 
-def iterate():
-    #Need to iterate through these words and match which AREN'T in stopwords list
-    user_words=user_text_and_clean()
-    stopwords_list=read_stopwords('stopwords.txt')
-    i = 0
-    n = len(list(user_words))
-    while i < n:
-        element = user_words[i]
-	if element in stopwords_list:
-            del user_words[i]
-            n = n-1
-        else:
-            i = i+1
-    return user_words
+    def get_stopwords_list(self):
+        return self.stopwords_list
+
+    def get_user_words(self):
+        return self.user_words
+
+    def iterate(self):
+        #Need to iterate through these words and match which AREN'T in stopwords list
+        i = 0
+        n = len(list(self.user_words))
+        while i < n:
+            element = self.user_words[i]
+            if element in self.stopwords_list:
+                del self.user_words[i]
+                n = n-1
+            else:
+                i = i+1
+        return self.user_words
 
 
-def display():
-    user_words = iterate()
-    wordcloud = wc.WordCloud(width=1000, height=500).generate(' '.join(list(user_words)))
+    def display(self):
+        wordcloud = wcld.WordCloud(width=1000, height=500).generate(' '.join(list(self.user_words)))
 
-    plt.figure(figsize=(15,8))
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.show()
+        plt.figure(figsize=(15,8))
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.show()
 
 
 if __name__ == "__main__":
-    display()
+    wc = Wordcloudifier('stopwords.txt',None)
+    wc.user_text_and_clean()
+    wc.iterate()
+    wc.display()
